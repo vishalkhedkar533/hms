09702fcfc23bd44a9b1c65c99f741cddb478aec7
Shyam P <shyam.p@nichebit.com>
2025-09-15 18:34:18 +0530

    Agent Search changes

--- Models/Models/DTO/Agent.cs
+++ Models/Models/DTO/Agent.cs
@@ -109,20 +109,67 @@ namespace Models.DTO
     {
         public string? SearchCondition { get; set; }
         public string? Zone { get; set; }
+        public int? AgentId { get; set; } = null!;
         public string? AgentCode { get; set; } = null!;
-        public string? AgentName { get; set; }
-        public string? ChannelCode { get; set; }
-        public string? SubChannelCode { get; set; }
-        public string? PanNumber { get; set; }
-        public string? Email { get; set; }
-        public string? MobileNo { get; set; }
-        public string? AadhaarNumber { get; set; }
-        public string? IrdaLicenseNumber { get; set; }
-        public string? GstNumber { get; set; }
+        //public string? AgentName { get; set; }
+        //public string? ChannelCode { get; set; }
+        //public string? SubChannelCode { get; set; }
+        //public string? PanNumber { get; set; }
+        //public string? Email { get; set; }
+        //public string? MobileNo { get; set; }
+        //public string? AadhaarNumber { get; set; }
+        //public string? IrdaLicenseNumber { get; set; }
+        //public string? GstNumber { get; set; }
         public Int64? PageNo { get; set; }
         public Int64? PageSize { get; set; }
         public string? SortColumn { get; set; }
         public string? SortDirection { get; set; }
 
+    }
+
+    public class AgentDtoResponse
+    {
+        public int agent_id { get; set; }
+        public string agent_code { get; set; } = null!;
+        public string? AgentTypeCode { get; set; }
+        public string? AgentSubTypeCode { get; set; }
+        public string? agent_name { get; set; }
+        public string? BusinessName { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Prefix { get; set; }
+        public string? Suffix { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? Nationality { get; set; }
+        public string? MaritalStatusCode { get; set; }
+        public string? PreferredLanguage { get; set; }
+        public string? ChannelCode { get; set; }
+        public string? SubChannelCode { get; set; }
+        public string? DesignationCode { get; set; }
+        public string? AgentLevel { get; set; }
+        public string? LocationCode { get; set; }
+        public string? StaffCode { get; set; }
+        public int? Supervisor_Id { get; set; }
+        public DateTime? ContractedDate { get; set; }
+        public string? AgentStatusCode { get; set; }
+        public DateTime? StatusDate { get; set; }
+        public bool IsLicensed { get; set; }
+        public string? MaskedPanNumber { get; set; }
+        public string? aadhaar_number { get; set; }
+        public string? irda_license_number { get; set; }
+        public string? gst_number { get; set; }
+        public string CreatedBy { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int? RowVersion { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string? pan_number { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNo { get; set; }
+        public int? total_count { get; set; }
+        
     }
 }

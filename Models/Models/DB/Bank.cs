71a4e6e3c21978a4de76f73e2d7d9d9ed874e16c
navinkondayur <navin@nichebit.com>
2025-11-19 17:20:32 +0530

    BankAccount

--- Models/Models/DB/Bank.cs
+++ Models/Models/DB/Bank.cs
@@ -1,21 +1,43 @@
 ﻿using Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models.DB
 {
+    [Table("BankAccount", Schema = "hms")]
     public class BankAccount
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int RefKey = 0;
-        public ReferenceType? RefType = ReferenceType.Agent;
+        [Required]
+        [Column(TypeName = "integer")]
+        public int RefKey { get; set; } = 0;
+        [Required]
+        [Column(TypeName = "integer")]
+        public int RefType { get; set; } = 1;
+        [Required]
+        [MaxLength(200)]
         public string AccountHolderName { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
         public string AccountNumber { get; set; } = null!;
+        [Required]
+        [MaxLength(20)]
         public string IFSC { get; set; } = null!;
-        public string MICR { get; set; } = null!;
+        [MaxLength(20)]
+        public string? MICR { get; set; } = null!;
+        [MaxLength(200)]
         public string? BankName { get; set; }
+        [MaxLength(200)]
         public string? BranchName { get; set; }
-        public BankAccType AccountType = BankAccType.Savings;
-        public DateTime? ActiveSince = DateTime.Now;
-        public string? FactoringHouse = null;
-        public PreferredPaymentMode preferredPaymentMode = PreferredPaymentMode.BankTransfer;
+        [Required]
+        public int AccountType { get; set; } = 1;
+        [Column(TypeName = "timestamp")]
+        public DateTime? ActiveSince { get; set; } = DateTime.Now;
+        [MaxLength(200)]
+        public string? FactoringHouse { get; set; }
+        [Required]
+        public PreferredPaymentMode PreferredPaymentMode { get; set; } = 1;
     }
 }

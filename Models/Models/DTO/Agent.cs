2484e3f92abf5c208a280d115fca00b33f4f8285
navinkondayur <navin@nichebit.com>
2025-12-06 11:37:43 +0530

    corrected

--- Models/Models/DTO/Agent.cs
+++ Models/Models/DTO/Agent.cs
@@ -139,7 +139,7 @@ namespace Models.DTO
         public string? AddressLine1 { get; set; }
         public string? AddressLine2 { get; set; }
         public string? AddressLine3 { get; set; }
-        public string? City { get; set; { get; set; }
+        public string? City { get; set; }
         public string? State { get; set; }
         public string? Country { get; set; }
         public string? Pin { get; set; }

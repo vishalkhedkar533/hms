c38324fa7b633e2703b3752fcf10fcfd5f2e2808
vishalkhedkar533 <officevk533@gmail.com>
2025-12-17 15:24:13 +0530

    updated-commission-api's

---
+++ Models/Models/DTO/CommissionMgmt/Dashboard/ProcessCommissionModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO.CommissionMgmt.Dashboard
+{
+    public class ProcessCommissionLogDto
+    {
+        public int ProcessId { get; set; }
+        public DateTime ProcessedDate { get; set; }
+        public string? Period { get; set; }
+        public int RecordsCount { get; set; }
+        public string? Status { get; set; }
+        public bool CanDownload { get; set; }
+        public bool CanViewDetails { get; set; }
+    }
+
+    public class ProcessCommissionResponseDto
+    {
+        public int OrgId { get; set; }
+        public string? PeriodType { get; set; }
+        public List<ProcessCommissionLogDto>? ProcessedRecordsLog { get; set; }
+    }
+}

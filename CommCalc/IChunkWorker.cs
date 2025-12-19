7cbdcaaef09327d19d93308b80bd31bbe7f220d8
navinkondayur <navin@nichebit.com>
2025-12-20 01:17:36 +0530

    dropped sample code

--- CommCalc/IChunkWorker.cs
+++
@@ -1,9 +0,0 @@
-namespace CommCalc.Jobs;
-
-public interface IChunkWorker
-{
-    /// <summary>
-    /// Processes a single chunk. Hangfire will resolve an instance from DI.
-    /// </summary>
-    Task ProcessChunk(int chunkIndex, int offset, int limit, Guid runId, CancellationToken cancellationToken = default);
-}

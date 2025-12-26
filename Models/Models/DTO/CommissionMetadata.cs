e25c61f51d2f86c573577529051dd8d69f0890e3
vishalkhedkar533 <officevk533@gmail.com>
2025-12-26 18:44:26 +0530

    commission-fields-json

---
+++ Models/Models/DTO/CommissionMetadata.cs
@@ -0,0 +1,16 @@
+﻿namespace Models.DTO
+{
+    public class CommissionMetadata
+    {
+        public FieldInfo[]? policy { get; set; }
+        public FieldInfo[]? premium { get; set; }
+        public FieldInfo[]? customer { get; set; }
+    }
+
+    public class FieldInfo
+    {
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public string? dataType { get; set; }
+    }
+}

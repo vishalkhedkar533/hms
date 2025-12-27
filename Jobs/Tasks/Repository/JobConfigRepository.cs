745ed97c026c0ac3a18610a5953e2418ae78bd31
navinkondayur <navin@nichebit.com>
2025-12-27 16:43:34 +0530

    connection string corrected

--- Jobs/Tasks/Repository/JobConfigRepository.cs
+++ Jobs/Tasks/Repository/JobConfigRepository.cs
@@ -10,17 +10,21 @@ namespace Repository
     {
         private readonly IConnectionFactory _connectionFactory;
         private readonly IMappingProvider _mappingProvider;
+        private readonly IConfiguration _configuration;
         public JobConfigRepository(IConnectionFactory connectionFactory, IConfiguration configuration
             , IMappingProvider mappingProvider)
         {
             _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
             _mappingProvider = mappingProvider ?? throw new ArgumentNullException(nameof(mappingProvider));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         public async Task<IEnumerable<JobConfig>> GetEnabledAsync()
         {
             OperationMapping operationMapping = _mappingProvider.GetScriptForOperation("Job", "Fetch");
-            await using var conn = (System.Data.Common.DbConnection)_connectionFactory.CreateConnection(operationMapping.ConnectionStringKey);
+            await using var conn = (System.Data.Common.DbConnection)_connectionFactory
+                .CreateConnection(_configuration
+                .GetConnectionString( operationMapping.ConnectionStringKey));
             await conn.OpenAsync();
             var rows = await conn.QueryAsync<JobConfig>(operationMapping.Script, commandType: CommandType.Text);
             return rows.ToList();

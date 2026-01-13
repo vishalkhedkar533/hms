a5513c223ad893ebc62c23e46e34f957c587d439
vishalkhedkar533 <officevk533@gmail.com>
2026-01-13 14:28:33 +0530

    create class for premium

---
+++ Jobs/Tasks/Insurance/PremiumUploadExcel.cs
@@ -0,0 +1,50 @@
+﻿using CommonLibrary.mapping;
+using Database;
+using Quartz;
+using Repository;
+
+namespace Tasks.Insurance
+{
+    public class PremiumUploadExcel
+    {
+        private readonly IJobExecutionContext _jobExecutionContext;
+        private int orgId = 0;
+        public JobKey jobKey;
+        private readonly ILogger<PolicyExcelUpload> _logger;
+        private readonly IMappingProvider _mappingProvider;
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+        private readonly IConnectionScope _connectionScope;
+        private readonly IBinaryImportFactory _bulkOpsFactory;
+
+        public PremiumUploadExcel(IJobExecutionContext jobExecutionContext,
+        ILogger<PolicyExcelUpload> logger,
+        IMappingProvider mappingProvider,
+        Microsoft.Extensions.Configuration.IConfiguration configuration,
+        IConnectionScope connectionScope,
+        IBinaryImportFactory bulkOpsFactory)
+        {
+            _jobExecutionContext = jobExecutionContext;
+            orgId = int.Parse(jobExecutionContext.JobDetail.JobDataMap.Values
+                .Select((value, index) => new { value, index })
+                .FirstOrDefault(x => x.index.Equals(
+                    jobExecutionContext.JobDetail.JobDataMap.Keys
+                    .Select((value, index) => new { value, index })
+                    .FirstOrDefault(x => x.value == "orgId").index))
+                .value.ToString());
+            jobKey = jobExecutionContext.JobDetail.Key;
+            _logger = logger;
+            _mappingProvider = mappingProvider ?? throw new ArgumentNullException(nameof(mappingProvider));
+
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            _connectionScope = connectionScope ?? throw new ArgumentNullException(nameof(connectionScope));
+
+            _bulkOpsFactory = bulkOpsFactory ?? throw new ArgumentNullException(nameof(bulkOpsFactory));
+        }
+
+        public async Task UploadPremiumData()
+        {
+
+        }
+    }
+}

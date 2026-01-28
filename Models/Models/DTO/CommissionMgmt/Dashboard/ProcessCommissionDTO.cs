f9eba41b5794ed49e0f097c9c28cd5bd11619d2c
vishalkhedkar533 <officevk533@gmail.com>
2026-01-28 10:05:41 +0530

    updated

--- Models/Models/DTO/CommissionMgmt/Dashboard/ProcessCommissionDTO.cs
+++ Models/Models/DTO/CommissionMgmt/Dashboard/ProcessCommissionDTO.cs
@@ -15,17 +15,49 @@
     }
     public class ProcessCommissionExcelDTO
     {
+        // Agent / Commission
         public int AgentId { get; set; }
-
         public string AgentName { get; set; } = null!;
-
         public string Channel { get; set; } = null!;
         public int PremiumCollectionId { get; set; }
-        public int PremiumAmount { get; set; }
+        public decimal PremiumAmount { get; set; }
         public string? Formula { get; set; }
-        public int CommissionAmount { get; set; }
+        public decimal CommissionAmount { get; set; }
         public string Status { get; set; } = null!;
         public string? Logs { get; set; }
+
+        // Premium Collected
+        public int PcPremiumCollectionId { get; set; }
+        public int PolicyRef { get; set; }
+        public DateTime? PremiumReceivedDate { get; set; }
+        public int? PremiumType { get; set; }
+        public int PremiumCollectionYear { get; set; }
+        public int PremiumCollectionQuarter { get; set; }
+        public string PremiumCollectionFinYear { get; set; } = null!;
+
+        // Policy
+        public string? PolicyNo { get; set; }
+        public string? PolicySuffix { get; set; }
+        public DateTime? RiskStartDate { get; set; }
+        public DateTime? RiskEndDate { get; set; }
+        public int? PolicyTerm { get; set; }
+        public int? PremiumPayingTerm { get; set; }
+        public string? ProposerClientId { get; set; }
+        public string? LifeInsuredClientId { get; set; }
+        public bool? IsStaffPolicy { get; set; }
+        public int? PolicySourceCode { get; set; }
+        public string? InsuredPAN { get; set; }
+        public string? ProposerPAN { get; set; }
+        public DateTime? InsuredDOB { get; set; }
+        public DateTime? ProposerDOB { get; set; }
+        public DateTime? LoginDate { get; set; }
+        public int? InsuredGender { get; set; }
+        public int? ProposerGender { get; set; }
+        public int? MaturityAgeInMonths { get; set; }
+        public decimal? ModalBasePremium { get; set; }
+        public decimal? ModalBaseRiderPremium { get; set; }
+        public string? ProductCode { get; set; }
     }
+
 
 }

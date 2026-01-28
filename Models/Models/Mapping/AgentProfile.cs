65bc7f0dc192df216add08cda7340d12100e5318
vishalkhedkar533 <officevk533@gmail.com>
2026-01-28 16:34:11 +0530

    spell-corrected-in-mapping

--- Models/Models/Mapping/AgentProfile.cs
+++ Models/Models/Mapping/AgentProfile.cs
@@ -125,10 +125,10 @@ namespace Models.Mapping
                 .ForMember(d => d.CandidateTypeDesc, s => s.Ignore())
                 .ForMember(d => d.AgentTypeDesc, s => s.Ignore())
                 .ForMember(d => d.CommissionClassDesc, s => s.Ignore())
-                .ForMember(d => d.LicenceType, o => o.MapFrom(s => s.LicenseType))
-                .ForMember(d => d.LicenceStatus, o => o.MapFrom(s => s.LicenseStatus))
-                .ForMember(d => d.LicenceTypeDesc, o => o.Ignore())
-                .ForMember(d => d.LicenceStatusDesc, o => o.Ignore())
+                .ForMember(d => d.LicenseType, o => o.MapFrom(s => s.LicenseType))
+                .ForMember(d => d.LicenseStatus, o => o.MapFrom(s => s.LicenseStatus))
+                .ForMember(d => d.LicenseTypeDesc, o => o.Ignore())
+                .ForMember(d => d.LicenseStatusDesc, o => o.Ignore())
                 .ForMember(d => d.VerticalDesc, o => o.Ignore())
                 .ForMember(d => d.TrainingGroupTypeDesc, o => o.Ignore())
                 
@@ -187,8 +187,8 @@ namespace Models.Mapping
                 .ForMember(d => d.AgentType, o => o.MapFrom(s => s.AgentType))
                 .ForMember(d => d.Country, o => o.MapFrom(s => s.Country))
 
-                .ForMember(d => d.LicenseType, o => o.MapFrom(s => s.LicenceType))
-                .ForMember(d => d.LicenseStatus, o => o.MapFrom(s => s.LicenceStatus))
+                .ForMember(d => d.LicenseType, o => o.MapFrom(s => s.LicenseType))
+                .ForMember(d => d.LicenseStatus, o => o.MapFrom(s => s.LicenseStatus))
                 .ForMember(d => d.Vertical, o => o.MapFrom(s => s.Vertical))
                 .ForMember(d => d.TrainingGroupType, o => o.MapFrom(s => s.TrainingGroupType))
                 ;

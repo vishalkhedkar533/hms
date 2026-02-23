2534ed5b6ff3439379f3fbaf944e63bacb0f9046
navinkondayur <navin@nichebit.com>
2026-02-23 16:52:45 +0530

    hms.sr_approver

--- DBScripts/PostgreSQL/DBScripts.sql
+++ DBScripts/PostgreSQL/DBScripts.sql
@@ -2468,6 +2468,7 @@ create table hms.inbox
 create table hms.sr_approver
 (
 	sr_approverId serial4 NOT NULL,
+	orgid int4 NOT NULL,
 	approverLevel int4 not null,
 	allocatedRoleId int4 not null,
 	decisionBy int4 NULL,
@@ -2476,5 +2477,6 @@ create table hms.sr_approver
 	approvalpayload text not null,
 	approvalapiresponse text not null,
     CONSTRAINT sr_approver_pkey PRIMARY KEY (sr_approverId),
+    CONSTRAINT ui_fields_setting_orgid_fkey FOREIGN KEY (orgid) REFERENCES app_subscription.organisation(orgid) ON DELETE cascade,
     CONSTRAINT sr_approver_decisionBy_fkey FOREIGN KEY (decisionBy) REFERENCES hms."user"(user_id) ON DELETE CASCADE
 );

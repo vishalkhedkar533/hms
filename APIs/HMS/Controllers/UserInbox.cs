95216ce060edcc1334be6575416024cc5b5a8688
navinkondayur <navin@nichebit.com>
2026-02-23 16:49:34 +0530

    UserInboxController

---
+++ APIs/HMS/Controllers/UserInbox.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using CommonLibrary;
+using HMS.Caching;
+using HMS.Data;
+using HMS.Services;
+
+namespace HMS.Controllers
+{
+    public class UserInbox
+    {
+        private readonly HMSContext _context;
+        private readonly GenericCacheService _cacheService;
+        private readonly IConfiguration _configuration;
+        private readonly IAuthClaimService _authClaimService;
+        private readonly FileService _fileService;
+        private readonly ILogger<UserInbox> _logger;
+        private readonly DatabaseService _db;
+        private readonly IMapper _mapper;
+
+        public UserInbox(HMSContext context, GenericCacheService cacheService, IConfiguration configuration
+            , IAuthClaimService authClaimService, FileService fileService, ILogger<UserInbox> logger,
+            DatabaseService db, IMapper mapper)
+        {
+            _cacheService = cacheService;
+            _configuration = configuration;
+            _authClaimService = authClaimService;
+            _fileService = fileService;
+            int refreshInterval = _configuration.GetValue<int>("Caching:refreshIntervalMinutes", 15);
+            _context = context;
+            _logger = logger;
+            _db = db;
+            _mapper = mapper;
+        }
+    }
+}
